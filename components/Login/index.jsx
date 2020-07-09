7da6de1d55c26daf0aaaa4590c01b3d93caf7800
imsourceboom <ecmascript.developer@gmail.com>
2020-07-09 16:51:08 +0900

    api test

--- components/Login/index.jsx
+++ components/Login/index.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const handleTelegramResponse = async response => {
   console.log(response);
   const data = await response;
-  return axios.post('http://localhost:8008/login', data);
+  return axios.post('http://localhost:8008/user', data);
 };
 
 export default () => {

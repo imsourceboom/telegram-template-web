dde6f00311f56f25a1d6a3aaab800bbd47ac708a
imsourceboom <ecmascript.developer@gmail.com>
2020-07-13 20:05:12 +0900

    credential test button

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,18 @@
 import Login from 'components/Login';
+import Axios from 'axios';
+
+const cookieTest = () => {
+  Axios.get('/', { withCredentials: true });
+};
 
 const Home = () => {
   return (
     <>
       <h1>Test!!!</h1>
       <Login />
+      <button type="button" onClick={() => cookieTest}>
+        버튼
+      </button>
     </>
   );
 };
